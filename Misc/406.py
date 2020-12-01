b6edb52fe26d1f894227812f772779ea16d37fd3
Kevin Tom <kevinktom@gmail.com>
2020-11-30 20:47:17 -0500

    Miscellaneous folder created for questions that don't follow a typical pattern and require specific logic

---
+++ Misc/406.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reconstructQueue(self, people):
+        """
+        :type people: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        
+        """
+        PseudoCode:
+            -sort by first element reversed, then second element ascending
+                -sort by the first element because you want to work with the taller people first since shorter people don't matter                  for their position
+            [7,0], [7,1], [6,1], [5,0], [5,2], [4,4]
+            -empty answer array
+            -loop through the above sorted array
+            -if the answer array is empty, append the element
+                -for the first element
+            -insert people at index of second element
+        """
+        people.sort(key=lambda x: (-x[0], x[1]))
+        answer = []
+        for person in people:
+            answer.insert(person[1], person)
+        return answer
